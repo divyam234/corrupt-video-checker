f808572890ec9bd8a87b602152e9a24e2820fbb0
divyam234 <47589864+divyam234@users.noreply.github.com>
2024-06-28 13:48:04 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module videocorrupt
+
+go 1.22.0
+
+require golang.org/x/sync v0.7.0
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type videoInfo struct {
+	Format struct {
+		Duration string `json:"duration"`
+	} `json:"format"`
+	Streams []struct {
+		CodecType string `json:"codec_type"`
+		Width     int
+		Height    int
+	} `json:"streams"`
+}
+
+func checkCorruptVideo(inputVideo string, interval int) error {
+
+	md5hash := md5.New()
+	md5hash.Write([]byte(inputVideo))
+	hash := md5hash.Sum(nil)
+	hex := hex.EncodeToString(hash)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "ffprobe", "-v", "error", "-show_format", "-show_streams", "-of", "json", inputVideo)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("could not get video info: %v", err)
+	}
+	vInfo := &videoInfo{}
+	err = json.Unmarshal([]byte(out), vInfo)
+	if err != nil {
+		return err
+	}
+
+	var duration float64
+	for _, s := range vInfo.Streams {
+		if s.CodecType == "video" {
+			duration, _ = strconv.ParseFloat(vInfo.Format.Duration, 64)
+		}
+	}
+
+	g, _ := errgroup.WithContext(ctx)
+
+	g.SetLimit(6)
+
+	for i := interval; i < int(duration); i += interval {
+		i := i
+		g.Go(func() error {
+			defer os.Remove(fmt.Sprintf("%s-%d.jpg", hex, i))
+			cmd := exec.CommandContext(ctx, "ffmpeg", "-hide_banner", "-y", "-ss", strconv.Itoa(i), "-i", inputVideo, "-v", "error", "-xerror", "-vframes", "1", fmt.Sprintf("%s-%d.jpg", hex, i))
+			out, err := cmd.CombinedOutput()
+			if err != nil && strings.Contains(string(out), "Invalid NAL unit size") {
+				cancel()
+				return fmt.Errorf("corrupted video")
+			} else if err != nil {
+				return err
+			}
+
+			return nil
+		})
+	}
+
+	return g.Wait()
+
+}
+
+func main() {
+	var (
+		filePath string
+		interval int
+	)
+
+	flag.StringVar(&filePath, "path", "", "File Path HTTP or Local")
+	flag.IntVar(&interval, "interval", 60, "Frame Interval in Seconds")
+
+	required := []string{"path"}
+
+	flag.Parse()
+
+	seen := make(map[string]bool)
+
+	flag.VisitAll(func(f *flag.Flag) {
+		if f.Value.String() != "" {
+			seen[f.Name] = true
+		}
+	})
+	for _, req := range required {
+		if !seen[req] {
+			fmt.Fprintf(os.Stderr, "missing required -%s argument\n", req)
+			os.Exit(2)
+		}
+	}
+	err := checkCorruptVideo(filePath, interval)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
